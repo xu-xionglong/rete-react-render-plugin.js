564f6710fe513720eb9c8e0163f6bf8f27a15270
许雄龙 <xionglong.xu@gmail.com>
2019-02-03 15:38:27 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,30 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function createNode(editor, { el, node, component, bindSocket, bindControl }) {
-	ReactDOM.render(
-	  React.createElement('div', null, 'Node'),
-	  el
-	);    
+
+class ReactNode extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        let inputs = this.props.node.inputs;
+        let reactInputs = [];
+        inputs.forEach((input, key, map) => {
+            reactInputs.push((<div ref={ (element) => {this.props.bindSocket(element, "input", input)} }>{input.key}</div>));
+
+        });
+        let outputs = this.props.node.outputs;
+        let reactOutputs = [];
+        outputs.forEach((output, key, map) => {
+            reactOutputs.push((<div ref={ (element) => {this.props.bindSocket(element, "output", output)} }>{output.key}</div>))
+        });
+        return (
+            <div>
+                {this.props.node.name}
+                {reactInputs}
+                {reactOutputs}
+            </div>
+        );
+    }
 }
 
-function createControl(editor, { el, control }) {
-	ReactDOM.render(
-	  React.createElement('div', null, 'Control'),
-	  el
-	);      
+function createNode(editor, { el, node, component, bindSocket, bindControl }) {
+    return ReactDOM.render(
+        <ReactNode 
+            node={node}
+            bindSocket={bindSocket}
+            bindControl={bindControl}
+        />,
+        el
+    );
 }
+
 
 function install(editor, params) {
     editor.on('rendernode', ({ el, node, component, bindSocket, bindControl }) => {
         if (component.render && component.render !== 'react') return;
-        createNode(editor, { el, node, component, bindSocket, bindControl });
+        let reactNode = createNode(editor, { el, node, component, bindSocket, bindControl });
     });
 
     editor.on('rendercontrol', ({ el, control }) => {
         if (control.render && control.render !== 'react') return;
-        createControl(editor, { el, control });
+
     });
+
+    editor.on("connectioncreated connectionremoved", connection => {
+        
+    });
+    editor.on("nodeselected", () => {
+
+    });  
 }
 
 export default {
